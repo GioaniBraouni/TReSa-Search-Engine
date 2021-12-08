343e83236178512de34c2fd24c5359c9532f47cd
GioaniBraouni <dit18131@office365.uop.gr>
2021-12-08 16:21:03 +0200

    Adding GUI

--- U.I/src/main/java/tresa/simulator/tresa_indexer/App.java
+++ U.I/src/main/java/tresa/simulator/tresa_indexer/App.java
@@ -20,12 +20,59 @@ import java.net.MalformedURLException;
  * JavaFX App
  */
 public class App extends Application
-{
-
+{private GridPane main = new GridPane();
+    private TextField searchBar = new TextField();
+    private String text = "";
+    private Stage stage;
 
     @Override
-    public void start(Stage stage) throws FileNotFoundException, MalformedURLException {
+    public void start(Stage stage) throws IOException
+    {
 
+        createLayout();
+
+        main.setStyle("-fx-background-color: white;");
+        var scene = new Scene(main, 1200, 768);
+
+        stage.setScene(scene);
+        stage.setTitle("TReSA");
+        stage.show();
+
+    }
+
+    private void createLayout()
+    {
+        VBox searchBox = new VBox(15);
+        ImageView image = new ImageView(new Image(("file:src/TRESA.png"),350,0,true,false));
+        searchBox.getChildren().add(image);
+        searchBox.setAlignment(Pos.TOP_CENTER);
+
+        searchBar.setPrefWidth(450);
+        searchBar.setFocusTraversable(false);
+        searchBar.setPromptText("Αναζήτηση στο TReSA");
+        searchBar.setPadding(new Insets(10, 10, 10, 10));
+        searchBox.getChildren().add(searchBar);
+        main.add(searchBox, 0, 0);
+
+        HBox buttons = new HBox(10);
+
+        Button search = new Button("Search");
+        search.setPadding(new Insets(10,30,10,30));
+        buttons.getChildren().add(search);
+
+        Button options = new Button("Options");
+        options.setPadding(new Insets(10,30,10,30));
+        buttons.getChildren().add(options);
+
+        buttons.setAlignment(Pos.CENTER);
+
+        main.add(buttons,0,1);
+        main.setVgap(10);
+        main.setAlignment(Pos.CENTER);
+    }
+
+    private void Options()
+    {
         final FileChooser fileChooser = new FileChooser();
 
         final DirectoryChooser directoryChooser = new DirectoryChooser();
@@ -50,7 +97,12 @@ public class App extends Application
 
         VBox box = new VBox(20);
 
-        Image image = new Image(new File("/home/dimitris/IdeaProjects/Testing_Project/U.I/src/main/java/tresa/simulator/tresa_indexer/images/not_google2.png").toURI().toURL().toExternalForm());
+        Image image = null;
+        try {
+            image = new Image(new File("/home/dimitris/IdeaProjects/Testing_Project/U.I/src/main/java/tresa/simulator/tresa_indexer/images/not_google2.png").toURI().toURL().toExternalForm());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
 
         ImageView imageView = new ImageView(image);
 
@@ -67,17 +119,6 @@ public class App extends Application
         forButtons.setAlignment(Pos.TOP_LEFT);
 
         borderPane.setTop(forButtons);
-
-
-
-
-
-
-        var scene = new Scene(borderPane, 800, 800);
-        stage.setScene(scene);
-        stage.setTitle("Not Google");
-        stage.show();
-
 
         addFiles.setOnAction(new EventHandler<ActionEvent>() { // Gets path of Files
             @Override
@@ -133,18 +174,9 @@ public class App extends Application
                 }
             }
         });
-
-
-
-
-
-
-
     }
 
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
-
-
 }
